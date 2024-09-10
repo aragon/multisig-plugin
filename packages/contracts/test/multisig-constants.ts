6a2c080836fa3340d8dc0b37c78f9225a1c2ee7b
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-09-10 21:31:09 +0400

    initializeFrom and erc165 changes for upgrades

--- packages/contracts/test/multisig-constants.ts
+++ packages/contracts/test/multisig-constants.ts
@@ -6,7 +6,6 @@ export const MULTISIG_EVENTS = {
 };
 
 export const MULTISIG_INTERFACE = new ethers.utils.Interface([
-  'function initialize(address,address[],tuple(bool,uint16),tuple(address,uint8))',
   'function updateMultisigSettings(tuple(bool,uint16))',
   'function createProposal(bytes,tuple(address,uint256,bytes)[],uint256,bool,bool,uint64,uint64)',
   'function getProposal(uint256)',
