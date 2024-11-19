faca10cc9acd39ef450a353f3a996f0ac0ed8054
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-11-19 10:57:14 +0400

    push

--- packages/contracts/docs/config.js
+++ packages/contracts/docs/config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
+const {version, repository} = require('../package.json');
+
+const helpers = require(path.resolve(__dirname, './templates/helpers'));
+
+// overwrite the functions.
+helpers.version = () => version;
+helpers.githubURI = () => repository.url;
+
 /** @type import('solidity-docgen/dist/config').UserConfig */
 module.exports = {
   outputDir: 'docs/modules/api/pages',
@@ -9,6 +17,6 @@ module.exports = {
   pageExtension: '.adoc',
   collapseNewlines: true,
   pages: (_, file, config) => {
-    return 'Multisig' + config.pageExtension;
+    return 'multisig' + config.pageExtension;
   },
 };
