a6bf367f4d2bc7e7a7b6f1f574dbbacc0406edf8
Jør∂¡ <itsbrickpop@gmail.com>
2025-02-06 13:55:52 +0100

    Automated JSON generation

--- packages/artifacts/prepare-abi.sh
+++ packages/artifacts/prepare-abi.sh
@@ -4,30 +4,38 @@ set -e  # Exit on error
 
 # Define directories
 CONTRACTS_FOLDER="../contracts"
-SOURCE_ABI_FILE="$CONTRACTS_FOLDER/artifacts/abi.ts"
-TARGET_ABI_FOLDER="./src"
+JSON_ABI_FOLDER="./src/abi"
+TARGET_ABI_FILE="./src/abi.ts"
 
 # Move into contracts package and install dependencies
 cd $CONTRACTS_FOLDER
 
-yarn install
-
-yarn build
-
-yarn prepare-abi
-
-if [ ! -f "$SOURCE_ABI_FILE" ]; then
-    echo "Error: $SOURCE_ABI_FILE not found."
-    exit 1
-fi
+yarn install && yarn build
 
 # Move back to artifacts package
-cd -
+cd - > /dev/null
 
 # Ensure the output directory exists
-mkdir -p "$TARGET_ABI_FOLDER"
+rm -Rf $JSON_ABI_FOLDER
+mkdir -p $JSON_ABI_FOLDER
 
-# Copy the generated ABIs to the output directory
-cp "$SOURCE_ABI_FILE" "$TARGET_ABI_FOLDER/abi.ts"
+# Extract the abi field into new JSON files
+for SRC_CONTRACT in $(ls $CONTRACTS_FOLDER/src/*.sol )
+do
+    FILE=$(basename $(echo $SRC_CONTRACT))
+    cat $CONTRACTS_FOLDER/artifacts/src/$FILE/${FILE%".sol"}.json | jq ".abi" > $JSON_ABI_FOLDER/${FILE%".sol"}.json
+done
 
-echo "ABI prepared: $TARGET_ABI_FOLDER/abi.ts"
+# Write the main ABI file
+rm -f $TARGET_ABI_FILE
+for ABI_FILE in $(ls $JSON_ABI_FOLDER)
+do
+    CONTRACT_NAME=${ABI_FILE%".json"}
+    echo "Importing $CONTRACT_NAME from $TARGET_ABI_FILE"
+
+    echo "import * as $CONTRACT_NAME from \"./abi/$ABI_FILE\";" >> $TARGET_ABI_FILE
+    echo "export {$CONTRACT_NAME}" >> $TARGET_ABI_FILE
+    echo "" >> $TARGET_ABI_FILE
+done
+
+echo "ABI prepared: $JSON_ABI_FOLDER"
