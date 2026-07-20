4fda69b46a95cd7302a3d49bfe7aa5b15586c356
Giorgi Lagidze <giorgilagidze0920@gmail.com>
2026-07-20 19:31:00 +0400

    prettier fix

--- packages/contracts/src/Multisig.sol
+++ packages/contracts/src/Multisig.sol
@@ -570,7 +570,10 @@ contract Multisig is
     /// @param _proposalId The ID of the proposal.
     /// @param _account The account to check.
     /// @return Returns `true` if the given account can approve on a certain proposal and `false` otherwise.
-    function _canApprove(uint256 _proposalId, address _account) internal view virtual returns (bool) {
+    function _canApprove(
+        uint256 _proposalId,
+        address _account
+    ) internal view virtual returns (bool) {
         Proposal storage proposal_ = proposals[_proposalId];
 
         if (!_isProposalOpen(proposal_)) {
